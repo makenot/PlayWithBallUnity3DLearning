570a7b7bbf4d886ad49dec3b67575de9264ab783
makenot <makenot@gmail.com>
2014-07-10 19:53:07 +0200

    - added: project file

---
+++ Assets/BallSpamBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+using AssemblyCSharp;
+
+public class BallSpamBehavior : MonoBehaviour {
+	public GameObject ballprefab;
+	// Use this for initialization
+	private  float timeToSpam=10;
+	private  float _currnentTime =0f;
+	void Start () {
+	
+	
+	
+	}
+
+	void spam(object sender, ElapsedEventArgs args){
+		Instantiate (ballprefab);
+	}
+	// Update is called once per frame
+	void Update () {
+		_currnentTime += Time.deltaTime; 
+		Debug.Log("_currnentTime:"+_currnentTime);
+		if(timeToSpam<_currnentTime)
+		{
+
+			_currnentTime=0;
+			spam(this,null);
+			Debug.Log("should spam");
+
+		}
+	}
+
+}
---
+++ Assets/MovePlatformBehavor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatformBehavor : MonoBehaviour {
+	public float sensity= 2f ;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		var force = new Vector3 (Input.GetAxis ("Horizontal") * sensity, Input.GetAxis ("Vertical") * sensity, 0);
+		this.rigidbody.velocity =force;
+		if ((force.x + force.y) > 0)
+						Debug.Log ("set felocity :"+force);
+	}
+}
